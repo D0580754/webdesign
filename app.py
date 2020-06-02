07157c5d370692b1c02061fd6635b3fd0e17bd0e
D0580754 <32447524+D0580754@users.noreply.github.com>
2020-06-02 16:30:37 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world."
+
+@app.route("/index")
+def index():
+    title="wanderer"
+    return render_template("index.html", title=title)
+
+if __name__ == "__main__":
+    app.run()
